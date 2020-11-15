4d16b071b9868e834f3956a2e5179eb84951a58a
leeyuxinxin <leeyuxinxin@163.com>
2020-11-15 11:20:35 +0800

    update 1

---
+++ springboot_mybatis_annotation/src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.example.demo.mapper;
+
+import com.example.demo.model.User;
+
+import java.util.List;
+
+public interface UserMapper {
+
+    List<User> getAll();
+
+    User getOne(Long id);
+
+    void insert(User user);
+
+    void update(User user);
+
+    void delete(Long id);
+}
